92ade865098f852a009099ed1ebe1461d5f9b8f5
SABINO KARI B <202100977b@utea.edu.pe>
2023-11-18 10:44:07 -0500

    App_youtube_8

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    _apiService.getVideos();
+    //_apiService.getVideos();
 
     return SingleChildScrollView(
       child: Padding(
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
+import 'package:flluter_codigo_yuoapp/models/video_model.dart';
 import 'package:http/http.dart' as http;
 
 class APIService {
-  getVideos() async {
+
+  Future<List<VideoModel>> getVideos() async {
+    List<VideoModel> videosModel = [];
     String _path =
         "https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAt7BSP8KRJwHXuvP6A6gdjXgdYHV6zhjQ&maxResults=20&regionCode=PE";
     Uri _uri = Uri.parse(_path);
@@ -11,7 +14,9 @@ class APIService {
     if (response.statusCode == 200) {
       Map<String, dynamic> myMap = json.decode(response.body);
       List videos = myMap["items"];
-      print(videos);
+      videosModel = videos.map((e) => videoModelFromJson(e)).toList();
+      return videosModel;
     }
+    return videosModel;
   }
 }
